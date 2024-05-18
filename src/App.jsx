1a8609a9e7808838cf93720e78633131a8d7bcc9
Clapzzy <martin.kurtew@gmail.com>
2024-05-18 05:53:41 +0300

    fixed submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import background from "../src/assets/background.png"
-import { useQueries } from '@tanstack/react-query'
-import { fetchKlekove, fetchKolelo, fetchKoremni, fetchLicevi } from './helper'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  async function boink(event) {
+    event.preventDefault();
+    await axios.post("http://3.77.19.140:3000/leaderboard/", {
+      name: event.target.name.value,
+      score: event.target.score.value,
+      type: event.target.type.value
+    })
+  }
 
   return (
     <div className=' flex flex-row  overflow-hidden justify-between px-6 w-full h-full bg-[#262626]' style={{ height: '100vh', backgroundImage: `url(${background})`, backgroundSize: 'cover' }} >
-      <form action='http://3.77.19.140:3000/leaderboard/' method='POST'>
+      <form onSubmit={boink} action='' method='POST'>
         <input required id='name' name='name' placeholder="name" type='text' />
         <input required id='score' name='score' placeholder='score' type='text' />
         <select id='type' name='type'>
